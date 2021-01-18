844633a6c366dc0be1d71665fd0628856d68b871
粽子 <rail_0039@126.com>
2021-01-19 02:00:58 +0800

    feat: 配置代理请求本地数据

--- src/views/TabBar/Detail.vue
+++ src/views/TabBar/Detail.vue
@@ -1,16 +1,52 @@
 <template>
   <div class="detail">
-    <router-link to="/about/detail/seller">商品</router-link>
-    <router-link to="/about/detail/goods">评论</router-link>
-    <router-link to="/about/detail/ratings">商家</router-link>
-    <router-view />
+    <!--    <div class="detail_head">-->
+    <!--      <img src="" alt="logo"/>-->
+    <!--    </div>-->
+    <div class="detail_container">
+      <div class="detail_item">
+        <router-link to="/about/detail/seller">商品</router-link>
+      </div>
+      <div class="detail_item">
+        <router-link to="/about/detail/goods">评论</router-link>
+      </div>
+      <div class="detail_item">
+        <router-link to="/about/detail/ratings">商家</router-link>
+      </div>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
 export default {
   name: "Detail",
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.detail_container {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+
+  .detail_item {
+    text-align: center;
+    flex: 1;
+
+    a {
+      width: 100%;
+      font-size: 14px;
+      color: rgb(77, 85, 93);
+      display: inline-block;
+
+      &.router-link-active,
+      .router-link-exact-active {
+        color: rgb(240, 20, 20);
+      }
+    }
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const path = require("path");
 const myTheme = path.resolve(__dirname, "src/styles/var.less");
+const orderData = require("./src/mock/order-data.json");
+const musicData = require("./src/mock/tab-data.json");
+const Seller = orderData.seller;
+const Goods = orderData.goods;
+const Ratings = orderData.ratings;
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -13,6 +19,20 @@ module.exports = {
     }
   },
   devServer: {
+    before: function(app, server, compiler) {
+      app.get('/order/seller', (req, res) => {
+        res.json({ data: Seller });
+      });
+      app.get('/order/goods', (req, res) => {
+        res.json({ data: Goods });
+      });
+      app.get('/order/ratings', (req, res) => {
+        res.json({ data: Ratings });
+      });
+      app.get('/music', (req, res) => {
+        res.json({ data: musicData });
+      });
+    },
     host: "0.0.0.0",
     port: 9056
   }
