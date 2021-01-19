5df6a62d9270cc7feae6b8c1ba259874ed3d984b
粽子 <rail_0039@126.com>
2021-01-20 00:33:12 +0800

    fix: 背景滤镜功能

--- src/views/TabBar/Order/components/OrderHeader.vue
+++ src/views/TabBar/Order/components/OrderHeader.vue
@@ -24,6 +24,9 @@
       <span class="bulletin-text ellipsis">{{ seller.bulletin }}</span>
       <van-icon size="12" name="arrow"/>
     </div>
+    <div class="background">
+      <img :src="seller.avatar"/>
+    </div>
   </div>
 </template>
 
@@ -45,82 +48,101 @@ export default {
 </script>
 
 <style lang="less" scoped>
-header {
+.header_containers {
   position: relative;
-  color: #fff;
-  background-color: #4b4138;
-  padding: 24px 12px 18px 24px;
+  overflow: hidden;
+  header {
+    color: #fff;
+    background: rgba(7, 17, 27, 0.5);
+    padding: 24px 12px 18px 24px;
 
-  img {
-    border-radius: 2px;
-    vertical-align: middle;
-  }
+    img {
+      border-radius: 2px;
+      vertical-align: middle;
+    }
 
-  .content {
-    margin-left: 16px;
-    vertical-align: middle;
-    display: inline-block;
+    .content {
+      margin-left: 16px;
+      vertical-align: middle;
+      display: inline-block;
 
-    .title {
-      margin: 2px 0 8px 0;
+      .title {
+        margin: 2px 0 8px 0;
 
-      .name {
-        font-size: 16px;
-        line-height: 18px;
-        font-weight: bold;
+        .name {
+          font-size: 16px;
+          line-height: 18px;
+          font-weight: bold;
+        }
+      }
+
+      .description {
+        margin-bottom: 10px;
+        line-height: 12px;
+        font-size: 12px;
+      }
+
+      .support {
+        .text {
+          line-height: 12px;
+          font-size: 10px;
+        }
       }
     }
 
-    .description {
-      margin-bottom: 10px;
-      line-height: 12px;
-      font-size: 12px;
-    }
+    .support-count {
+      position: absolute;
+      right: 12px;
+      bottom: 40px;
+      height: 24px;
+      display: inline;
+      padding: 0 8px;
+      text-align: center;
+      line-height: 24px;
+      border-radius: 12px;
+      background: rgba(0, 0, 0, 0.2);
 
-    .support {
-      .text {
-        line-height: 12px;
+      .count {
+        vertical-align: middle;
         font-size: 10px;
       }
+
+      i {
+        margin-left: 2px;
+        vertical-align: middle;
+      }
     }
   }
 
-  .support-count {
-    position: absolute;
-    right: 12px;
-    bottom: 14px;
-    height: 24px;
-    display: inline;
-    padding: 0 8px;
-    text-align: center;
-    line-height: 24px;
-    border-radius: 12px;
-    background: rgba(0, 0, 0, 0.2);
+  .bulletin-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 28px;
+    line-height: 28px;
+    padding: 0 12px 0 12px;
+    color: #fff;
+    background: rgba(7, 17, 27, 0.6);
 
-    .count {
-      vertical-align: middle;
+    .bulletin-text {
+      margin: 0 4px;
       font-size: 10px;
     }
-
-    i {
-      margin-left: 2px;
-      vertical-align: middle;
-    }
   }
-}
 
-.bulletin-wrapper {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  height: 28px;
-  line-height: 28px;
-  padding: 0 12px 0 12px;
-  background: rgba(7, 17, 27, 0.2);
+  .background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -10;
+    filter: blur(10px);
 
-  .bulletin-text {
-    margin: 0 4px;
-    font-size: 10px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
   }
 }
 </style>
