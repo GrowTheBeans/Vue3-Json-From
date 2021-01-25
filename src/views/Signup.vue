36e5758f6e729a1cee1f6582be12c0e0a6a6767f
粽子 <rail_0039@126.com>
2021-01-25 23:52:44 +0800

    feat: 注册页面

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="signup_container">
-    注册页面
+    <div class="signup_left"></div>
+    <div class="signup_right">
+      <h3>欢迎注册粽子系统</h3>
+      <el-form label-position="top">
+        <el-form-item label="名称">
+          <el-input></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-input></el-input>
+        </el-form-item>
+        <el-form-item label="活动形式">
+          <el-input></el-input>
+        </el-form-item>
+        <el-switch />
+        <el-form-item size="large">
+          <el-button type="primary" size="small">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -10,3 +28,43 @@ export default defineComponent({
   name: 'Signup'
 })
 </script>
+
+<style lang="less" scoped>
+.signup_container {
+  display: flex;
+  flex-wrap: wrap;
+  align-content: space-between;
+  .signup_left {
+    width: 50%;
+    height: 100vh;
+    background: url("../components/image/groups@2x.png") no-repeat;
+  }
+  .signup_right {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 50%;
+    background: url("../components/image/BG@2x.png") no-repeat bottom;
+    background-size: 100%;
+    h3 {
+      text-align: center;
+      font-size: 30px;
+      color: #056fff;
+      line-height: 56px;
+      font-family: PingFangSC, PingFangSC-Medium;
+      font-weight: 500;
+    }
+    .el-form {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      width: 500px;
+      height: 650px;
+      .el-button {
+        margin-top: 20px;
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
